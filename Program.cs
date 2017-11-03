d66fd91cccb9a1d67410c644db1a6a1f25875971
Adhir Ramjiawan <adhir@lab.co.uk>
2017-11-03 09:55:07 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CommonServiceLocator;
+using SolrNet;
+using SolrNet.Attributes;
+
+namespace learnsolr
+{
+
+    public class Product 
+    {
+        [SolrUniqueKey("id")]
+        public string Id {get;set;}
+
+        [SolrField("manu")]
+        public string Manufacturer {get;set;}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Startup.Init<Product>("http://localhost:8983/solr/learnsolr");
+
+            var p = new Product{
+                Id = Guid.NewGuid().ToString(),
+                Manufacturer = "Sony"
+            };
+
+            var solr = ServiceLocator.Current.GetInstance<ISolrOperations<Product>>();
+            solr.Add(p);
+            solr.Commit();
+        }
+    }
+}
